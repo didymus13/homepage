ae6de73a0dc2b43ca1fbc7bd3749e198db0e5e1f
Stephane Doiron <stephane.doiron@b367.ca>
2019-02-15 14:27:30 -0500

    fixed radio buttons

--- pages/kids/math/config.vue
+++ pages/kids/math/config.vue
@@ -15,23 +15,26 @@
     </b-form-group>
 
     <b-form-group label="Use total for limit" horizontal>
-      <b-form-radio-group :value="config.useTotalForLimit" @change="$store.commit('math/setConfig', {field: 'useTotalForLimit', value: $event})">
-        <b-form-radio :value="true">Yes</b-form-radio>
-        <b-form-radio :value="false">No</b-form-radio>
+      <b-form-radio-group
+        :checked="config.useTotalForLimit"
+        :options="options"
+        @change="$store.commit('math/setConfig', {field: 'useTotalForLimit', value: $event})">
       </b-form-radio-group>
     </b-form-group>
 
     <b-form-group label="Whole answers only" horizontal>
-      <b-form-radio-group :value="config.wholeAnswersOnly" @change="$store.commit('math/setConfig', {field: 'wholeAnswersOnly', value: $event})">
-        <b-form-radio :value="true">Yes</b-form-radio>
-        <b-form-radio :value="false">No</b-form-radio>
+      <b-form-radio-group
+        :checked="config.wholeAnswersOnly"
+        :options="options"
+        @change="$store.commit('math/setConfig', {field: 'wholeAnswersOnly', value: $event})">
       </b-form-radio-group>
     </b-form-group>
 
     <b-form-group label="Positive answers only" horizontal>
-      <b-form-radio-group :value="config.positiveAnswersOnly" @change="$store.commit('math/setConfig', {field: 'positiveAnswersOnly', value: $event})">
-        <b-form-radio :value="true">Yes</b-form-radio>
-        <b-form-radio :value="false">No</b-form-radio>
+      <b-form-radio-group
+        :checked="config.positiveAnswersOnly"
+        :options="options"
+        @change="$store.commit('math/setConfig', {field: 'positiveAnswersOnly', value: $event})">
       </b-form-radio-group>
     </b-form-group>
   </section>
@@ -40,6 +43,12 @@
 <script>
 import { mapState } from 'vuex'
 export default {
+  data() {
+    return {
+      options: [{text: 'Yes', value: true}, {text: 'No', value: false}]
+    }
+  },
+
   computed: {
     ...mapState('math', ['config'])
   }
