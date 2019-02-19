193559db5b5a841b4b722855255bcbc7122c2580
Stephane Doiron <stephane.doiron@b367.ca>
2019-02-19 16:15:37 -0500

    dotenv installed

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,7 +44,8 @@ module.exports = {
     // Doc: https://github.com/nuxt-community/axios-module#usage
     '@nuxtjs/axios',
     // Doc: https://bootstrap-vue.js.org/docs/
-    'bootstrap-vue/nuxt'
+    'bootstrap-vue/nuxt',
+    '@nuxtjs/dotenv'
   ],
   /*
   ** Axios module configuration
