ac89fd20d8ed6a6d5e548d98059cfa7679276329
Stephane Doiron <stephane@stephanedoiron.com>
2019-03-04 10:09:54 -0500

    netlify attempt again (#2)
    * netlify attempt again
    
    * rename dance
    
    * rename dance
    
    * b-form-radio-group doesn't like SSR :/ Might have to switch to buefy
    
    * env vs process env?
    
    * Not really DRY

--- pages/hobbies/on-the-paint-table.vue
+++ pages/hobbies/on-the-paint-table.vue
@@ -30,6 +30,7 @@ import moment from 'moment'
 
 export default {
   async asyncData( {$axios, env, app} ) {
+    
     let paintingReq = $axios.$get(`${env.TRELLO_API}/lists/${env.TRELLO_LISTS_PAINTING}/cards`)
     let finishedReq = $axios.$get(`${env.TRELLO_API}/lists/${env.TRELLO_LISTS_FINISHED}/cards`)
     const [paintingRes, finishedRes] = await Promise.all([paintingReq, finishedReq])
