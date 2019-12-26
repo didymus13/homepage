845d779a5f5e470760a4b44e8eec8e6ae6b54c1d
Stephane Doiron <didymus1313@gmail.com>
2019-12-26 11:25:03 -0500

    netlify might only know html

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,28 +1,38 @@
-<template lang="pug">
-  div
-    v-parallax(src="/contact.jpg")
-      h1.display-1 Contact
+<template lang="html">
+  <div>
+    <v-parallax src="/contact.jpg">
+      <h1 class="display-1">Contact</h1>
+    </v-parallax>
 
-    v-container
-      v-alert(type="info")
-        | Quick note: I am not looking for new freelance work at this time. I'll
-        | remove this message when my schedule opens up.
+    <v-container>
+      <v-alert type="info">
+        Quick note: I am not looking for new freelance work at this time. I'll
+        remove this message when my schedule opens up.
+      </v-alert>
+    </v-container>
 
-      p
-        | Do you want to get in touch with me? <strong>That's great news!</strong>
-        | All you need to do is just fill out the form below and I'll get in
-        | touch as soon as possible.
+    <p>
+      Do you want to get in touch with me? <strong>That's great news!</strong>
+      All you need to do is just fill out the form below and I'll get in
+      touch as soon as possible.
+    </p>
 
-      form(name="contact" method="POST" netlify netlify-recaptcha)
-        v-row
-          v-col
-            v-text-field(label="Your name" name="name" outlined)
-          v-col
-            v-text-field(label="Your e-mail address" name="email" outlined)
-        v-textarea(label="How can I help you?" placeholder="Your awesome idea" name="request" rows="10" outlined)
-        v-textarea(label="Extra information" placeholder="List websites that you like, similar examples or exta information that might help get the ball rolling" name="extra" rows="5" outlined)
-        div(netlify-recaptcha)
-        v-btn(color="blue" type="submit" dark) Send in your request!
+    <form name="contact" method="POST" netlify netlify-recaptcha>
+      <v-row>
+        <v-col>
+          <v-text-field label="Your name" name="name" outlined />
+        </v-col>
+        <v-col>
+          <v-text-field label="Your e-mail address" name="email" outlined />
+        </v-col>
+      </v-row>
+
+      <v-textarea label="How can I help you?" placeholder="Your awesome idea" name="request" rows="10" outlined />
+      <v-textarea label="Extra information" placeholder="List websites that you like, similar examples or exta information that might help get the ball rolling" name="extra" rows="5" outlined />
+      <div netlify-recaptcha />
+      <v-btn color="blue" type="submit" dark> Send in your request! </v-btn>
+    </form>
+  </div>
 </template>
 
 <script>
