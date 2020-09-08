d79cb28eb80993c666cd22dc7149c6a39eb9d4dc
Stephane Doiron <didymus1313@gmail.com>
2020-09-08 13:32:19 -0400

    Added proper code rendering on pages

--- pages/posts/_slug.vue
+++ pages/posts/_slug.vue
@@ -12,6 +12,27 @@
 <script>
 import { first } from 'lodash'
 import { documentToHtmlString } from '@contentful/rich-text-html-renderer'
+import { BLOCKS } from '@contentful/rich-text-types'
+import hljs from 'highlight.js'
+
+const richTextOptions = {
+  renderNode: {
+    [BLOCKS.PARAGRAPH]: (node, next) => {
+      const content = next(node.content)
+      if (
+        node.nodeType == 'paragraph' &&
+        node.content.length === 1 &&
+        node.content[0].marks.length === 1 &&
+        node.content[0].marks[0].type === 'code'
+      ) {
+        const content = hljs.highlightAuto(node.content[0].value).value
+        // return content;
+        return `<pre><code class="hljs">${content}</code></pre>`
+      }
+      return `<p>${next(node.content)}</p>`
+    }
+  }
+}
 
 export default {
   head() {
@@ -53,7 +74,7 @@ export default {
 
   filters: {
     render(richText) {
-      return documentToHtmlString(richText)
+      return documentToHtmlString(richText, richTextOptions)
     }
   }
 }
